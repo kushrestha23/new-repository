89262b80956d0da64823b3610190f61f919ba145
kushrestha23 <35647717+kushrestha23@users.noreply.github.com>
2022-03-18 14:07:13 -0400

    Adding test cases

--- cypress/integration/api-tests/RestAPI.spec.js
+++ cypress/integration/api-tests/RestAPI.spec.js
@@ -1,46 +1,101 @@
 describe("Testing API Endpoints using Cypress", () => {
+  let id = 10;
   it("Test GET Request", () => {
-    cy.request(
-      "https://originalfarm.com/wp-json/wp/v3/menu-products?per_page=10"
-    ).then((response) => {
-      expect(response.body).to.have.property('code', 200);
+    cy.request("https://jsonplaceholder.typicode.com/users").then(
+      (response) => {
+        expect(response.body.length).to.eq(id);
+        id++;
+      }
+    );
+  });
+
+  it("this should fail", () => {
+    cy.request("https://jsonplaceholder.typicode.com/users").then(
+      (response) => {
+        expect(response.body.length).to.eq(id);
+        id++;
+      }
+    );
+  });
+
+  describe("Testing POST Request", () => {
+    it("POST Request", () => {
+      cy.request({
+        method: "POST",
+        url: "https://jsonplaceholder.typicode.com/posts",
+        body: JSON.stringify({
+          title: "foo",
+          body: "bar",
+          userId: 1,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }).then((response) => {
+        expect(response.status).to.eq(201);
+        expect(response.body).to.have.property("title", "foo");
+        expect(response.body).to.have.property("body", "bar");
+        expect(response.body).to.have.property("userId", 1);
+      });
     });
   });
 
-  // it("Test POST Request", () => {
-  //   cypress
-  //     .request({
-  //       method: "POST",
-  //       url: "https://localhost:3000/api/posts",
-  //       body: {
-  //         id: 2,
-  //         title: "Automation",
-  //       },
-  //     })
-  //     .then((response) => {
-  //       expect(response.body).has.property("title", "Automation");
-  //     });
-  // });
+  it("Trying the PUT Request", () => {
+    cy.request({
+      method: "PUT",
+      url: "https://jsonplaceholder.typicode.com/posts/1",
+      body: JSON.stringify({
+        id: 1,
+        title: "Sujesh",
+        body: "Shrestha",
+        userId: 1,
+      }),
+      headers: {
+        "content-type": "application/json; charset=UTF-8",
+      },
+    }).then((response) => {
+      expect(response.body).to.have.property("title", "Sujesh");
+    });
+  });
 
-  // it("Test PUT Request", () => {
-  //   cy.request({
-  //     method: "PUT",
-  //     url: "https://localhost:3000/api/posts/2",
-  //     body: {
-  //       id: 2,
-  //       title: " Test Automation",
-  //     },
-  //   }).then((response) => {
-  //     expect(response.body).has.property("title", "Test Automation");
-  //   });
-  // });
+  it("Trying the Patch Request", () => {
+    cy.request({
+      method: "Patch",
+      url: "https://jsonplaceholder.typicode.com/posts/1",
+      body: JSON.stringify({
+        title: "Saurav",
+        userId: 1,
+      }),
+      headers: {
+        "content-type": "application/json; charset=UTF-8",
+      },
+    }).then((response) => {
+      expect(response.body).to.have.property("title", "Saurav");
+    });
+  });
 
-  // it("Test DELETE Request", () => {
-  //   cy.request({
-  //     method: "DELETE",
-  //     url: "https://localhost:3000/api/posts/2",
-  //   }).then((response) => {
-  //     expect(response.body).to.be.empty;
-  //   });
-  // });
+  it("Trying the DELETE Request TEST CASE PASSED", () => {
+    cy.request({
+      method: "DELETE",
+      url: "https://jsonplaceholder.typicode.com/posts/2",
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+    });
+  });
+
+  it("Trying the DELETE Request TEST CASE FAILED", () => {
+    cy.request({
+      method: "DELETE",
+      url: "https://jsonplaceholder.typicode.com/posts/2",
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+    });
+  });
+  it("Trying filter records through API", () => {
+    cy.request("https://jsonplaceholder.typicode.com/posts/1").then(
+      (response) => {
+        expect(response.body.length).to.eq(id);
+      }
+    );
+  })
 });
